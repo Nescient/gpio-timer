7df5725d6e85596d9fcba48461d5279cf78b8c63
nescient <antimetabole@gmail.com>
2023-06-13 23:03:08 -0400

    feat: added restclient-cpp

--- source/lib.cpp
+++ source/lib.cpp
@@ -1,8 +1,13 @@
 #include "lib.hpp"
 
 #include <fmt/core.h>
+#include <iostream>
+
+#include "restclient-cpp/restclient.h"
 
 library::library()
     : name {fmt::format("{}", "gpio")}
 {
+    RestClient::Response r = RestClient::get("http://example.com");
+    std::cout << r.body << std::endl;
 }
