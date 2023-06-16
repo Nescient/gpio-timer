3a13ed01ea546bdd61892bbcf8c001228f2670c0
nescient <antimetabole@gmail.com>
2023-06-16 00:06:50 -0400

    feat: added gpiod and timers

--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/antchfx/xmlquery v1.3.16 // indirect
 	github.com/antchfx/xpath v1.2.4 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/loov/hrtime v1.0.3 // indirect
+	github.com/warthog618/gpiod v0.8.1 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
---
+++ gpiod/gpiod.go
@@ -0,0 +1,20 @@
+// gpiod is a wrapper package using warthog618's gpiod to watch specific GPIO devices for
+// changes.  this is the main functionality of gpio-timer
+package gpiod
+
+import (
+	"github.com/warthog618/gpiod"
+	"github.com/loov/hrtime"
+)
+
+var startGpio = 10
+var lane1Gpio = 11
+var lane2Gpio = 11
+var lane3Gpio = 11
+var lane4Gpio = 11
+
+// GetGateTime will watch the start GPIO and return a high-precision
+// time value for when it starts
+func GetGateTime() time.Duration, int64 {
+	return hrtime.Now(), hrtime.TSC()
+}
