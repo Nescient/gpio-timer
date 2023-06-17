f1e7c9836dc00cb1a51e5ad04eb17cb1c7bdad66
nescient <antimetabole@gmail.com>
2023-06-16 22:13:23 -0400

    fix: fixed compile issues

--- gpio/gpio.go
+++ gpio/gpio.go
@@ -5,6 +5,8 @@ package gpio
 import (
 	"github.com/warthog618/gpiod"
 	"github.com/loov/hrtime"
+	"time"
+	"log"
 )
 
 var startGpio = 10
@@ -13,8 +15,30 @@ var lane2Gpio = 11
 var lane3Gpio = 11
 var lane4Gpio = 11
 
+
+// init will 
+func init() {
+	
+}
+
 // GetGateTime will watch the start GPIO and return a high-precision
 // time value for when it starts
-func GetGateTime() time.Duration, int64 {
+func GetGateTime() (time.Duration, hrtime.Count) {
 	return hrtime.Now(), hrtime.TSC()
 }
+
+func handler(evt gpiod.LineEvent) {
+	// handle edge event
+  }
+  
+  func x() {
+	c, err := gpiod.NewChip("gpiochip2")
+	if err != nil{
+		log.Fatal(err)
+	}
+	log.Println(c);
+//   l, _ := c.RequestLine(rpi.J8p7, gpiod.WithEventHandler(handler), gpiod.WithBothEdges)
+//   in, _ := gpiod.RequestLine("gpiochip0", 2, gpiod.AsInput)
+// val, _ := in.Value()
+// out, _ := gpiod.RequestLine("gpiochip0", 3, gpiod.AsOutput(val))
+  }
