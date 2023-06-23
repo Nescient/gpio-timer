e94542ff15b62ed7ea9976dd3f103dcede3d40d2
nescient <antimetabole@gmail.com>
2023-06-22 23:51:36 -0400

    fix: switched to gpio3 from gpio1

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@
 
 docker run --net=host --rm -v "$PWD":/usr/src/myapp \
    -w /usr/src/myapp -e HOME=/usr/src/myapp --device=/dev/gpiochip1 \
-   --device=/dev/gpiochip2 golang:1.20 go run github.com/Nescient/gpio-timer
+   --device=/dev/gpiochip2 --device=/dev/gpiochip3 --device=/dev/gpiochip4 \
+   golang:1.20 go run github.com/Nescient/gpio-timer
 
