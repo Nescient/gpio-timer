1b5c5df9cf87988c2b23e3bff34ff28838fb0a2a
nescient <antimetabole@gmail.com>
2023-06-24 19:42:24 -0400

    feat: docker now restart the crashed program

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 ./go.sh generate
 # ./go.sh run github.com/Nescient/gpio-timer
 
-docker run --net=host --rm -v "$PWD":/usr/src/myapp \
-   -w /usr/src/myapp -e HOME=/usr/src/myapp --device=/dev/gpiochip1 \
-   --device=/dev/gpiochip2 --device=/dev/gpiochip3 --device=/dev/gpiochip4 \
+docker run -itd --name derby-timer --restart unless-stopped --net=host \
+   -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e HOME=/usr/src/myapp \
+   --device=/dev/gpiochip1 --device=/dev/gpiochip2 --device=/dev/gpiochip3 --device=/dev/gpiochip4 \
    golang:1.20 go run github.com/Nescient/gpio-timer
 
+echo "To see output in the terminal, run"
+echo "docker logs derby-timer"
