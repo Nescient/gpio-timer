752fadac5f09b2ad642c214de95d1420963cbf02
nescient <antimetabole@gmail.com>
2023-06-24 21:45:57 -0400

    chore: updated script to remove old container

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 ./go.sh generate
 # ./go.sh run github.com/Nescient/gpio-timer
 
+docker stop derby-timer
+docker rm derby-timer
+
 docker run -itd --name derby-timer --restart unless-stopped --net=host \
    -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e HOME=/usr/src/myapp \
    --device=/dev/gpiochip1 --device=/dev/gpiochip2 --device=/dev/gpiochip3 --device=/dev/gpiochip4 \
